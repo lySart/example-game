8013ab2e0579d7f60d03fe46f35e74833720100a
lyS <youyou19990@live.com>
2021-09-06 21:05:37 +0800

    Create Portal

---
+++ Test_3DRpg_Game/Assets/Scripts/Transition/TransitionPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionPoint : MonoBehaviour
+{
+    public enum TransitionType
+    {
+        SameScene, DifferentScene
+    }
+
+    [Header("Transition Info")]
+    public string sceneName;
+    public TransitionType transitionType;
+
+    public TransitionDestination.DestinationTag destinationTag;
+
+    private bool canTrans;
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canTrans = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            canTrans = false;
+        }
+    }
+}
